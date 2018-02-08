114c8e6593b96cf8afbae39d40dc09fe23ea8658
Nouwatin Jacob <jaysansa@gmail.com>
2018-02-08 17:55:12 +0100

    -- profile model and seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 
 use App\User;
+use App\Profile;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,10 +14,19 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
+       $user = User::create([
             'name' => 'Nouwatin Jacob',
             'email' => 'jaysansa@gmail.com',
-            'password' => bcrypt('jayjay')
+            'password' => bcrypt('jayjay'),
+            'admin' => 1
+        ]);
+
+        Profile::create([
+            'user_id' => $user->id,
+            'avatar' => 'uploads/avatars/user.jpg',
+            'about' => 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. ',
+            'facebook' => 'facebook.com',
+            'youtube' => 'youtube.com'
         ]);
     }
 }
